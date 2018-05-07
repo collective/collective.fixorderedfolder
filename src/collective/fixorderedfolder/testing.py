ecfc780600e3f86bc826a19d6e9af84563b6f1be
Godefroid Chapelle <gotcha@bubblenet.be>
2018-05-07 14:38:33 +0200

    first import

---
+++ src/collective/fixorderedfolder/testing.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneWithPackageLayer
+from plone.testing import z2
+
+import collective.fixorderedfolder
+
+
+COLLECTIVE_FIXORDEREDFOLDER_FIXTURE = PloneWithPackageLayer(
+    zcml_package=collective.fixorderedfolder,
+    zcml_filename='testing.zcml',
+    name='CollectiveFixorderedfolderLayer',
+    additional_z2_products=()
+)
+
+
+COLLECTIVE_FIXORDEREDFOLDER_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(COLLECTIVE_FIXORDEREDFOLDER_FIXTURE,),
+    name='CollectiveFixorderedfolderLayer:IntegrationTesting'
+)
+
+
+COLLECTIVE_FIXORDEREDFOLDER_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(COLLECTIVE_FIXORDEREDFOLDER_FIXTURE,),
+    name='CollectiveFixorderedfolderLayer:FunctionalTesting'
+)
